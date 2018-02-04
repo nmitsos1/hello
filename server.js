e415ccd868027dd81c71c3a812041bda669de8fa
Nick Mitsos <nmitsos1@umbc.edu>
2018-02-04 11:42:31 -0500

    hello world with env

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+var PORT = process.env.PORT;
+
 var express = require('express');
 var app = express();
 
 // var app is now an http server
 
 // parameter 1 is a port number
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Server is up and running on port 3000');
 });
 
